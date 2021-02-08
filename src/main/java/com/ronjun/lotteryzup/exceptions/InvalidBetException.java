0113a303717eae46fca11e798f54e3381fae373c
Ronaldo Junior <ronaldocosta2603@gmail.com>
2021-02-07 22:51:51 -0300

    Project done

--- src/main/java/com/ronjun/lotteryzup/exceptions/InvalidBetException.java
+++
@@ -1,9 +0,0 @@
-package com.ronjun.lotteryzup.exceptions;
-
-public class InvalidBetException extends RuntimeException {
-	private static final long serialVersionUID = -7239378382478671588L;
-
-	public InvalidBetException(String msg) {
-		super(msg);
-	}
-}
